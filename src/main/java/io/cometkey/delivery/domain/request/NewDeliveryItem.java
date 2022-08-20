147a021fc72335747bc7d864981fed84a932efd4
AllyHyeseongKim <ally3k@icloud.com>
2022-08-20 13:39:43 +0900

    [feature/delivery] Create the request
    src/main/java/io/cometkey/delivery/domain/request/NewDelivery.java
    src/main/java/io/cometkey/delivery/domain/request/NewDeliveryItem.java

---
+++ src/main/java/io/cometkey/delivery/domain/request/NewDeliveryItem.java
@@ -0,0 +1,24 @@
+package io.cometkey.delivery.domain.request;
+
+import lombok.*;
+
+import javax.validation.constraints.NotNull;
+
+@Getter @Setter
+@NoArgsConstructor(access =  AccessLevel.PROTECTED)
+public class NewDeliveryItem {
+
+    @NotNull
+    private String name;
+    @NotNull
+    private Integer price;
+    @NotNull
+    private Integer quantity;
+
+    @Builder
+    public NewDeliveryItem(String name, Integer price, Integer quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
