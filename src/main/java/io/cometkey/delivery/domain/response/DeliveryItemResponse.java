b8cb3f595306d1b1bd2f6b2f6b49913b1174f1b7
AllyHyeseongKim <ally3k@icloud.com>
2022-08-20 13:40:17 +0900

    [feature/delivery] Create the response
    src/main/java/io/cometkey/delivery/domain/response/DeliveryItemResponse.java
    src/main/java/io/cometkey/delivery/domain/response/DeliveryResponse.java

---
+++ src/main/java/io/cometkey/delivery/domain/response/DeliveryItemResponse.java
@@ -0,0 +1,19 @@
+package io.cometkey.delivery.domain.response;
+
+import lombok.*;
+
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class DeliveryItemResponse {
+
+    private String name;
+    private Integer price;
+    private Integer quantity;
+
+    @Builder
+    public DeliveryItemResponse(String name, Integer price, Integer quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
