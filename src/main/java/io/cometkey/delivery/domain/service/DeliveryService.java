a38340cd5495b31ccb944ac741462875bb42dace
AllyHyeseongKim <ally3k@icloud.com>
2022-08-20 13:40:57 +0900

    [feature/delivery] Create the service
    src/main/java/io/cometkey/delivery/domain/service/DeliveryService.java

---
+++ src/main/java/io/cometkey/delivery/domain/service/DeliveryService.java
@@ -0,0 +1,29 @@
+package io.cometkey.delivery.domain.service;
+
+
+import io.cometkey.delivery.domain.Delivery;
+import io.cometkey.delivery.domain.DeliveryItem;
+import io.cometkey.delivery.domain.repository.DeliveryItemRepository;
+import io.cometkey.delivery.domain.repository.DeliveryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class DeliveryService {
+
+    private final DeliveryRepository deliveryRepository;
+    private final DeliveryItemRepository deliveryItemRepository;
+
+    public Delivery getDelivery(Long deliveryId) { return this.deliveryRepository.findById(deliveryId).get(); }
+
+    public void addNewDelivery(Delivery delivery, List<DeliveryItem> deliveryItems) {
+        for (DeliveryItem orderItem : deliveryItems) {
+            delivery.addDeliverItem(orderItem);
+        }
+        this.deliveryRepository.save(delivery);
+        this.deliveryItemRepository.saveAll(deliveryItems);
+    }
+}
