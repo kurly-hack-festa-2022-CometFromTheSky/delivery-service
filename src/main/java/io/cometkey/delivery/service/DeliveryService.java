de5c24053afe3e6b4c2e093f54f5c5ebf826fd41
AllyHyeseongKim <ally3k@icloud.com>
2022-08-22 13:50:38 +0900

    [feature/delivery] Modify the service
    src/main/java/io/cometkey/delivery/service/DeliveryService.java

--- src/main/java/io/cometkey/delivery/service/DeliveryService.java
+++ src/main/java/io/cometkey/delivery/service/DeliveryService.java
@@ -1,29 +1,19 @@
 package io.cometkey.delivery.service;
 
-
-import io.cometkey.delivery.domain.Delivery;
-import io.cometkey.delivery.domain.DeliveryItem;
-import io.cometkey.delivery.repository.DeliveryItemRepository;
-import io.cometkey.delivery.repository.DeliveryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class DeliveryService {
 
-    private final DeliveryRepository deliveryRepository;
-    private final DeliveryItemRepository deliveryItemRepository;
 
-    public Delivery getDelivery(Long deliveryId) { return this.deliveryRepository.findById(deliveryId).get(); }
+    public String getEncryptedKey(Long keyId) {
+        //TODO: worker에 해당 id encryptedKey GET 요청
+        return null;
+    }
 
-    public void addNewDelivery(Delivery delivery, List<DeliveryItem> deliveryItems) {
-        for (DeliveryItem orderItem : deliveryItems) {
-            delivery.addDeliverItem(orderItem);
-        }
-        this.deliveryRepository.save(delivery);
-        this.deliveryItemRepository.saveAll(deliveryItems);
+    public void expireUsedKey(Long keyId) {
+        //TODO: worker에 해당 id isUsed = true POST 요청
     }
 }
