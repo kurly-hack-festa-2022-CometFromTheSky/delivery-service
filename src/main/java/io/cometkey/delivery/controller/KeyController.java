6f692e6f554051a98a245eab729de2e612c77a83
AllyHyeseongKim <ally3k@icloud.com>
2022-08-23 01:57:40 +0900

    [feature/kafka] Create the producer

---
+++ src/main/java/io/cometkey/delivery/controller/KeyController.java
@@ -0,0 +1,29 @@
+package io.cometkey.delivery.controller;
+
+import io.cometkey.delivery.response.KeyResponse;
+import io.cometkey.delivery.service.KeyService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+public class KeyController {
+
+    private final KeyService keyService;
+
+    @GetMapping("/v1/emulator/{token}")
+    public KeyResponse GetDeliveryInfo(@Valid @PathVariable String token) throws Exception {
+
+        return KeyResponse.builder()
+                .encryptedKey(this.keyService.getEncryptedKey(token))
+                .build();
+    }
+
+    @PutMapping("/v1/emulator/{token}")
+    public void PutDeliveryInfo(@Valid @PathVariable String token) {
+
+        this.keyService.expireUsedKey(token);
+    }
+}
