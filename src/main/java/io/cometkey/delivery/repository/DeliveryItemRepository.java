a6954c20837628766c73b9aab6cb84551e014d45
AllyHyeseongKim <ally3k@icloud.com>
2022-08-22 13:53:02 +0900

    [feature/delivery] Modify the repository
    src/main/java/io/cometkey/delivery/repository/DeliveryItemRepository.java
    src/main/java/io/cometkey/delivery/repository/DeliveryRepository.java

--- src/main/java/io/cometkey/delivery/repository/DeliveryItemRepository.java
+++
@@ -1,7 +0,0 @@
-package io.cometkey.delivery.repository;
-
-import io.cometkey.delivery.domain.DeliveryItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DeliveryItemRepository extends JpaRepository<DeliveryItem, Long> {
-}
--- src/main/java/io/cometkey/delivery/repository/DeliveryRepository.java
+++
@@ -1,7 +0,0 @@
-package io.cometkey.delivery.repository;
-
-import io.cometkey.delivery.domain.Delivery;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DeliveryRepository extends JpaRepository<Delivery, Long> {
-}
